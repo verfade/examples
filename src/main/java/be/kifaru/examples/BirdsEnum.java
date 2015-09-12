1b882fd9bd83f25b303d6b9060e1790a867fd6c6
Dévid Verfaillie <verfade@users.noreply.github.com>
2015-09-12 19:43:14 +0200

    Add Enum example.

---
+++ src/main/java/be/kifaru/examples/BirdsEnum.java
@@ -0,0 +1,103 @@
+package be.kifaru.examples;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Devid Verfaillie
+ * @since 2015-09-01
+ */
+public enum BirdsEnum {
+
+    COMMON_KESTREL("common kestrel", "Falco tinnunculus"),
+
+    // someone duplicates a line and forgets to change the values => should be caught by sanity checks below
+//    COMMON_KESTREL_COPY("common kestrel", "Falco tinnunculus"),
+
+    ROOSTER("rooster", "Gallus gallus"),
+
+    WHITE_THROATED_DIPPER("white-throated dipper",
+                          "someone changed the original value instead of the value in the copy below"
+                                  + " => should be caught by BirdsEnumTest#test_knownEnumData_shouldBeValid()"),
+    WHITE_THROATED_DIPPER_COPY("a unique name", "Cinclus cinclus"),
+
+    WHITE_STORK("white stork", "Ciconia ciconia"),
+    // always have a comma after the last value for easy addition and merging
+
+    // keep this ';' on an empty line for easy merging
+    ;
+
+    /**
+     * Contains the common name and species (Strings) to Enum mappings.
+     */
+    private static final Map<String, BirdsEnum> MAPPINGS;
+
+    static {
+        Map<String, BirdsEnum> mappings = new HashMap<>();
+
+        for (BirdsEnum enumValue : BirdsEnum.values()) {
+            BirdsEnum sanityCheck = mappings.put(enumValue.commonName, enumValue);
+
+            if (null != sanityCheck) {
+                String errorMessage = "BirdsEnum common name [%s] from enum [%s] already exists for enum [%s]";
+                throw new AssertionError(String.format(errorMessage, enumValue.commonName, enumValue, sanityCheck));
+            }
+
+            sanityCheck = mappings.put(enumValue.species, enumValue);
+
+            if (null != sanityCheck) {
+                String errorMessage = "BirdsEnum species [%s] from enum [%s] already exists for enum [%s]";
+                throw new AssertionError(String.format(errorMessage, enumValue.species, enumValue, sanityCheck));
+            }
+        }
+
+        MAPPINGS = Collections.unmodifiableMap(mappings);
+    }
+
+    public static BirdsEnum fromString(String key) {
+        return MAPPINGS.get(key);
+    }
+
+    public static BirdsEnum fromStringNotNull(String key) {
+        BirdsEnum result = fromString(key);
+        if (null != result) {
+            return result;
+        }
+
+        throw new IllegalArgumentException(String.format("Unknown search string [%s]", key));
+    }
+
+    private final String commonName;
+
+    private final String species;
+
+    BirdsEnum(String commonName, String species) {
+        this.commonName = commonName;
+        this.species = species;
+    }
+
+    /**
+     * See Effective Java 2nd Edition, item 10 (Always override toString).
+     */
+    @Override
+    public String toString() {
+        // Enum#toString only contains the Enum name. You should override #toString to add more information
+        // and hopefully no-one will use #toString when they want #name.
+
+        return "BirdsEnum{" +
+                // make sure to add the enum name to the string
+                "name='" + name() + '\'' +
+                ", commonName='" + commonName + '\'' +
+                ", species='" + species + '\'' +
+                '}';
+    }
+
+    public String getCommonName() {
+        return commonName;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+}
---
+++ src/test/java/be/kifaru/examples/BirdsEnumTest.java
@@ -0,0 +1,87 @@
+package be.kifaru.examples;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static be.kifaru.examples.BirdsEnum.COMMON_KESTREL;
+import static be.kifaru.examples.BirdsEnum.ROOSTER;
+import static be.kifaru.examples.BirdsEnum.fromString;
+import static be.kifaru.examples.BirdsEnum.fromStringNotNull;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Tests {@link BirdsEnum}.
+ *
+ * @author Devid Verfaillie
+ * @since 2015-09-01
+ */
+public class BirdsEnumTest {
+
+    // constant for readability
+    private static final String NULL_STRING = null;
+
+    // constant for readability
+    private static final String EMPTY_STRING = "";
+
+    // constant for readability
+    private static final String BLANK_STRING = " ";
+
+    @BeforeClass
+    public static void testEnumInitialization() {
+        assertThat(COMMON_KESTREL, is(notNullValue()));
+        // At this moment the Enum class is loaded and there was no initializer error.
+
+        // uncomment COMMON_KESTREL_COPY in the enum to test this case
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void fromStringNotNull_blankArguments_shouldThrowIllegalArgumentException() {
+        fromStringNotNull(BLANK_STRING);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void fromStringNotNull_emptyArguments_shouldThrowIllegalArgumentException() {
+        fromStringNotNull(EMPTY_STRING);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void fromStringNotNull_nullArguments_shouldThrowIllegalArgumentException() {
+        fromStringNotNull(NULL_STRING);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void fromStringNotNull_unknownArguments_shouldThrowIllegalArgumentException() {
+        fromStringNotNull("dummy data");
+    }
+
+    @Test
+    public void fromString_blankArgument_shouldReturnNull() {
+        assertThat(fromString(BLANK_STRING), is(nullValue()));
+    }
+
+    @Test
+    public void fromString_emptyArgument_shouldReturnNull() {
+        assertThat(fromString(EMPTY_STRING), is(nullValue()));
+    }
+
+    @Test
+    public void fromString_nullArgument_shouldReturnNull() {
+        assertThat(fromString(NULL_STRING), is(nullValue()));
+    }
+
+    @Test
+    public void fromString_unknownArgument_shouldReturnNull() {
+        assertThat(fromString("dummy data"), is(nullValue()));
+    }
+
+    @Test
+    public void test_knownEnumData_shouldBeValid() {
+        assertThat(ROOSTER.getCommonName(), is("rooster"));
+        assertThat(ROOSTER.getSpecies(), is("Gallus gallus"));
+
+        // uncomment to test this case, the enum values are already prepared
+//        assertThat(BirdsEnum.WHITE_THROATED_DIPPER.getCommonName(), is("white-throated dipper"));
+//        assertThat(BirdsEnum.WHITE_THROATED_DIPPER.getSpecies(), is("Cinclus cinclus"));
+    }
+}

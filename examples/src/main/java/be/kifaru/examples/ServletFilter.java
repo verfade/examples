fbdbfe6bfdba0f61aa70f61975b9bc5971d147be
Dévid Verfaillie <verfade@users.noreply.github.com>
2015-11-30 20:02:05 +0100

    Add Filter example.

---
+++ examples/src/main/java/be/kifaru/examples/ServletFilter.java
@@ -0,0 +1,76 @@
+package be.kifaru.examples;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static java.lang.Boolean.TRUE;
+
+/**
+ * @author Devid Verfaillie
+ * @since 2015-11-18
+ */
+public class ServletFilter implements Filter {
+
+    // @VisibleForTesting
+    static final String ONCE_PER_REQUEST = ServletFilter.class.getName() + ".FILTERED";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // no implementation needed
+    }
+
+    protected void doBeforeNextFilter(ServletRequest servletRequest, ServletResponse servletResponse) {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        // put business logic here...
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        if (hasOncePerRequestAttribute(servletRequest)) {
+            continueWithNextFilter(servletRequest, servletResponse, filterChain);
+        } else {
+            setOncePerRequestAttribute(servletRequest);
+
+            try {
+                doBeforeNextFilter(servletRequest, servletResponse);
+                continueWithNextFilter(servletRequest, servletResponse, filterChain);
+            } finally {
+                executeFinallyClause(servletRequest);
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+        // no implementation needed
+    }
+
+    private void continueWithNextFilter(ServletRequest servletRequest,
+                                        ServletResponse servletResponse,
+                                        FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    protected void executeFinallyClause(ServletRequest request) {
+        request.removeAttribute(ONCE_PER_REQUEST);
+    }
+
+    private boolean hasOncePerRequestAttribute(ServletRequest servletRequest) {
+        return null != servletRequest.getAttribute(ONCE_PER_REQUEST);
+    }
+
+    private void setOncePerRequestAttribute(ServletRequest servletRequest) {
+        servletRequest.setAttribute(ONCE_PER_REQUEST, TRUE);
+    }
+}
---
+++ examples/src/test/java/be/kifaru/examples/ServletFilterTest.java
@@ -0,0 +1,70 @@
+package be.kifaru.examples;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static be.kifaru.examples.ServletFilter.ONCE_PER_REQUEST;
+import static java.lang.Boolean.TRUE;
+import static org.mockito.BDDMockito.*;
+
+/**
+ * Tests {@link ServletFilter}.
+ *
+ * @author Devid Verfaillie
+ * @since 2015-11-30
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ServletFilterTest {
+
+    @Mock(answer = Answers.RETURNS_SMART_NULLS)
+    private HttpServletRequest request;
+
+    @Mock(answer = Answers.RETURNS_SMART_NULLS)
+    private HttpServletResponse response;
+
+    @Mock(answer = Answers.RETURNS_SMART_NULLS)
+    private FilterChain filterChain;
+
+    // the System-Under-Test
+    private ServletFilter servletFilter;
+
+    @Before
+    public void before() throws Exception {
+        given(request.getAttribute(ONCE_PER_REQUEST)).willReturn(null);
+
+        servletFilter = new ServletFilter();
+    }
+
+    @Test
+    public void doFilter_requestWithOncePerRequestAttribute_shouldOnlyInvokeNextFilterInChain() throws Exception {
+        // GIVEN
+        given(request.getAttribute(ONCE_PER_REQUEST)).willReturn(TRUE);
+
+        // WHEN
+        servletFilter.doFilter(request, response, filterChain);
+
+        // THEN
+        then(request).should(only()).getAttribute(ONCE_PER_REQUEST);
+        then(filterChain).should(only()).doFilter(request, response);
+        verifyNoMoreInteractions(request, response, filterChain);
+    }
+
+    @Test
+    public void doFilter_requestWithoutOncePerRequestAttribute_shouldSetTheAttribute() throws Exception {
+        // GIVEN
+
+        // WHEN
+        servletFilter.doFilter(request, response, filterChain);
+
+        // THEN
+        then(request).should(times(1)).setAttribute(ONCE_PER_REQUEST, TRUE);
+    }
+}
